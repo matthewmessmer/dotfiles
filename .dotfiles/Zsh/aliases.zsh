e243cf5c82875756e0a2988d4e7bb32999c9c5c8
matthewmessmer <info@matthewmessmer.com>
2026-07-27 11:38:24 +0900

    Update aliases.zsh

--- .dotfiles/Zsh/aliases.zsh
+++ .dotfiles/Zsh/aliases.zsh
@@ -2,7 +2,7 @@
 
 # Eza/ls alias
 if [ -x "$(command -v eza)" ]; then
-    alias ls="eza -lahF --color-scale"
+    alias ls="eza -lahF --color-scale --icons --group-directories-first --git"
 fi
 
 alias reload!='. ~/.zshrc'
